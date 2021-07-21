1cf9fa75d11fe41855e67508a1213e1e7a4c05c9
Loyola <juanloyola@hotmail.com>
2021-07-21 17:16:15 -0300

    profile card component

--- src/components/profile/UserCard.vue
+++ src/components/profile/UserCard.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="UserCard bg-white -mt-20 ml-8">
-    <div class="row">
-      <div class="imgBox">
-        <img src="#" alt="Profile picture">
+  <div class="UserCard bg-white -mt-20 ml-8 shadow-lg">
+    <div class="row flex w-full h-full flex-row mr-2 justify-items-center items-center">
+      <div class="imgBox w-1/2 flex justify-center">
+        <img class="image w-1/2 h-full rounded-full bg-white shadow-lg border-transparent cursor-pointer" src="@/assets/profile.png" alt="Profile picture">
       </div>
 
-      <div class="infoB">
-        <p class="fullName"></p>
-        <p class="role"></p>
-        <p class="adress"></p>
-        <p class="adressTwo"></p>
+      <div class="infoBox flex flex-col justify-items-start items-start">
+        <p class="fullName font-semibold">{{name}} {{lastname}}</p>
+        <p class="role">{{role}}</p>
+        <p class="adress mt-3 text-gray-500">{{adress}}</p>
+        <p class="adressTwo text-gray-500">{{adressTwo}}</p>
       </div>
     </div>
   </div>
@@ -17,7 +17,22 @@
 
 <script>
 export default {
-  name: 'UserCard'
+  name: 'UserCard',
+
+  data () {
+    const firstName = this.$store.state.name
+    const lastName = this.$store.state.lastName
+    let role = this.$store.state.role
+    let adress = this.$store.state.adress
+    let adressTwo = this.$store.state.adressTwo
+    return {
+      name: firstName,
+      lastname: lastName,
+      role: role,
+      adress: adress,
+      adressTwo: adressTwo
+    }
+  }
 }
 </script>
 
@@ -26,5 +41,9 @@ export default {
   width:  750px;
   height: 350px;
   border-radius: 9px;
+  .image:hover {
+    filter:contrast(0.7);
+    border: dashed 1px rgb(165, 163, 163);
+  }
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,10 @@ import { createStore } from 'vuex'
 export default createStore({
   state: {
     name: 'Juan',
-    lastName: 'Loyola'
+    lastName: 'Loyola',
+    role: 'Front end developer',
+    adress:'4000 Santa Fe Avenue',
+    adressTwo:'Buenos Aires, Argentina'
   },
   mutations: {
   },
